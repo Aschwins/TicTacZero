a598213d815325816109b674b23d997f0641f691
aschilperoort <aschilperoort@xccelerated.io>
2019-10-30 21:35:20 +0100

    adding play n fast brainy games

--- src/tictaczero/games.py
+++ src/tictaczero/games.py
@@ -1,5 +1,6 @@
 import numpy as np
 from tictaczero.board import Board, CROSS, CIRCLE
+from tictaczero.player import BrainPlayer
 
 def play_a_game(Player1, Player2, print_result = False):
     """
@@ -7,14 +8,12 @@ def play_a_game(Player1, Player2, print_result = False):
     Returns: board_history
     """
     board = Board(3)
-    p1 = Player1
-    p2 = Player2
     
     board_history = np.zeros(shape=(1,9), dtype=np.int32)
     ongoing = True
     
     while ongoing:
-        for p in [p1, p2]:
+        for p in [Player1, Player2]:
             boardstate, winner, game_ended = p.move(board)
             board_history = np.concatenate((board_history, [boardstate]), axis = 0)
             if game_ended:
@@ -28,3 +27,38 @@ def play_a_game(Player1, Player2, print_result = False):
         board.print_board()
         
     return board_history
+
+
+def play_n_brainy_games(n_games=100):
+    """
+    Plays n games and returns both (hopefully) smarter players.
+    """
+    player1 = BrainPlayer(side=CROSS)
+    player2 = BrainPlayer(side=CIRCLE)
+    board = Board(3)
+    
+    board_history = np.empty(shape = (9,10), dtype = np.int32)
+    
+    for i in range(n_games):
+        board_history.fill(0)
+        board.reset()
+        n_moves = 0
+        ongoing = True
+        
+        while ongoing:
+            for p in [player1, player2]:
+                boardstate, winner, game_ended = p.move(board)
+                board_history[n_moves, :9] = boardstate
+                n_moves += 1
+                
+                if game_ended:
+                    ongoing = False
+                    board_history[0:n_moves, 9] = winner
+                    break
+                    
+                
+        player1.memorize(board_history[0:n_moves, :])
+        player2.memorize(board_history[0:n_moves, :])
+                    
+    return player1, player2
+
